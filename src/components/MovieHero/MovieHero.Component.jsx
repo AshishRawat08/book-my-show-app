4b898c8f10d50b2eef792a78cb4fcc9680544beb
AshishRawat08 <rawashu18@gmail.com>
2023-12-29 11:21:35 +0530

    movie-page,context,movienavbar

--- src/components/MovieHero/MovieHero.Component.jsx
+++ src/components/MovieHero/MovieHero.Component.jsx
@@ -1,32 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import { MovieContext } from "../../context/Movie.context";
 import MovieInfo from "./MovieInfo.Component";
 
 const MovieHero = () => {
   const { movie } = useContext(MovieContext);
 
-  const genres = movie.genres?.map(({ name }) => name).join(",");
+  const genres = movie.genres?.map(({ name }) => name).join(", ");
+
   // console.log(genres);
   return (
     <>
       <div>
-        {/* for small and medium (mobile n tab)screen size  */}
+        {/* Mobile and Tab Screen i.e., small and medium screen size */}
         <div className="lg:hidden w-full">
           <img
-            src={`https://image.tmbd.org/t/p/original${movie.poster_path}`}
+            src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
             alt="Cover Poster"
             className="m-4 rounded"
             style={{ width: "calc(100%-2rem)" }}
           />
         </div>
-
         <div className="flex flex-col gap-3 lg:hidden">
           <div className="flex flex-col-reverse gap-3 px-4 my-3">
             <div className="text-black flex flex-col gap-2 md:px-4">
               <h4>4.4k rating</h4>
-              <h4>kannada, English, Telugu, Hindi, Tamil</h4>
+              <h4>Kannada, English, Hindi, Telgu, Tamil</h4>
               <h4>
-                {movie.runtime}min | {genres}
+                {movie.runtime} min | {genres}
               </h4>
             </div>
           </div>
@@ -39,7 +39,8 @@ const MovieHero = () => {
             </button>
           </div>
         </div>
-        {/* for large (laptop desktop)screen size  */}
+        {/* ctrl+/ */}
+        {/* Large Screen Size i.e., Laptop */}
         <div
           className="relative hidden w-full lg:block"
           style={{ height: "28rem" }}
@@ -48,15 +49,16 @@ const MovieHero = () => {
             className="absolute z-10 w-full h-full"
             style={{
               backgroundImage:
-                "linear-gradient(90deg, rgb(34, 34 ,34) 24.95%, rbg(34, 34, 34) 38.3%, rgba(34, 34, 34, 0.04) 97.47%, rgba(34, 34, 34) 100%)",
+                "linear-gradient(90deg, rgb(34,34,34) 24.95%, rgb(34,34,34) 38.2%, rgba(34,34,34,0.03) 97.47%, rgb(34, 34, 34) 100%)",
             }}
           >
             <div className="absolute z-30 left-24 top-10 flex items-center gap-10">
               <div className="w-64 h-96">
                 <img
-                  src={`https://image.tmbd.org/t/p/original${movie.poster_path}`}
+                  src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
                   alt="Movie Poster"
-                  className="w-full h-full rounded-lg"
+                  className="w-full h-full rounded-lg
+                "
                 />
               </div>
               <div>
@@ -64,8 +66,8 @@ const MovieHero = () => {
               </div>
             </div>
             <img
-              src={`https://image.tmbd.org/t/p/original${movie.backdrop_path}`}
-              alt="Backgroup poster"
+              src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+              alt="Backdrop poster"
               className="w-full h-full object-cover object-center"
             />
           </div>
